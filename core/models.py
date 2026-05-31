3e3bc09602d7e340a052ad75c7e6cc358fd79d14
notsuz <ifosuz77@gmail.com>
2026-05-31 16:57:42 +0545

    dynamic adding projects and designes

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+    title=models.CharField(max_length=50)
+    image=models.ImageField(upload_to='images')
+    caption=models.CharField(max_length=200)
+     
+    def __str__(self):
+        return self.title
+    
+class UiDesign(models.Model):
+    title=models.CharField(max_length=50)
+    image=models.ImageField(upload_to='images')
+    caption=models.CharField(max_length=200)
+     
+    def __str__(self):
+        return self.title
--- core/views.py
+++ core/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import Projects,UiDesign
 
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    
+    project=Projects.objects.all()
+    uidesign=UiDesign.objects.all()
+    
+    context={
+        'project':project,
+        'uidesign':uidesign
+    }
+    return render(request, 'core/index.html',context)
+
+
